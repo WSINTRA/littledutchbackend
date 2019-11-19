75d07dd472a982fa164caacd327bd23886cf8eaa
WSINTRA <will.simpson85@gmail.com>
2019-11-19 17:53:56 -0500

    Created review controller and tested with front end, controller can now create new reviews

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,5 +1,5 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id, :title, :user_id, :product_id, :content
+  attributes :id, :title, :user_id, :product_id, :content, :product
   belongs_to :user
   belongs_to :product
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,9 @@
 Rails.application.routes.draw do
-  namespace :v1 do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    namespace :v1 do
     resources :orders, only: [:create, :update, :show]
     post 'orders/newOrder'
     get 'orders/allOrders'
-  end
-  namespace :v1 do
-    get 'admin_menu/sales'
-    get 'admin_menu/orders'
-    get 'admin_menu/customers'
-    get 'admin_menu/employees'
-  end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    namespace :v1 do
 	resources :users, only: [:create, :update, :show]
       post '/login', to: 'auth#create'
       get '/profile', to: 'users#profile'
@@ -20,6 +12,15 @@ Rails.application.routes.draw do
       post '/new_product', to: 'products#create'
       get '/all_products', to: 'products#index'
       patch '/update_products', to: 'products#update'
+      resources :review, only: [:create, :update, :show]
+       post 'review/new'
+       patch 'review/edit'
+      delete 'review/delete'
+      resources :admin_menu, only: [:show]
+          get 'admin_menu/sales'
+    get 'admin_menu/orders'
+    get 'admin_menu/customers'
+    get 'admin_menu/employees'
     end
 
 end
