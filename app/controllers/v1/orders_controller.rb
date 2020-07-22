9461ea69ba0991198dca7858168cb737258b74a0
WSINTRA <will.simpson85@gmail.com>
2020-07-22 13:56:10 -0400

    updated orders_controller with new property labels, itemId, itemTitle etc

--- app/controllers/v1/orders_controller.rb
+++ app/controllers/v1/orders_controller.rb
@@ -5,14 +5,18 @@ class V1::OrdersController < ApplicationController
   	total_price = 0
   	#work out total first
   	#create a new order with customer id and total price
-  	#create purchases for each item in the order using the new order number
-  	params["order"].each do |x| total_price += x["price"].to_f end
+    #create purchases for each item in the order using the new order number
+    params["order"].each do 
+      |x| total_price += x["itemPrice"].to_f 
+    end
+
     @order = Order.create(user_id: params["userId"], total: total_price)
     params["order"].each do |x| Purchase.create(order_id: @order.id, 
-    	product_id: x["id"],
+
+    	product_id: x["itemId"],
     	quantity: 1) 
     end
-  	render json: {"response": "Order created"}
+  	render json: {"response": "Order created", "order": @order}
   end
 
   def allOrders
